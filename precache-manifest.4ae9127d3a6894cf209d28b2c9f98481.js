8dd97dfbe1ee1a00bd386ee4e7f69a926935b8ca
Maxim Dmitriev <Renissance@tut.by>
2022-05-29 18:20:03 +0300

    Updates

---
+++ precache-manifest.4ae9127d3a6894cf209d28b2c9f98481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1856d761be073d4a4d4a044107ee8c",
+    "url": "/it-incubator-todolist-ts/index.html"
+  },
+  {
+    "revision": "55d60f70574201a2325d",
+    "url": "/it-incubator-todolist-ts/static/css/main.b0d87614.chunk.css"
+  },
+  {
+    "revision": "8687e3895d0165189ec3",
+    "url": "/it-incubator-todolist-ts/static/js/2.38b57ddc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/it-incubator-todolist-ts/static/js/2.38b57ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d60f70574201a2325d",
+    "url": "/it-incubator-todolist-ts/static/js/main.1d10b7e7.chunk.js"
+  },
+  {
+    "revision": "1ab7487ce92f6fb728b6",
+    "url": "/it-incubator-todolist-ts/static/js/runtime-main.f8216a61.js"
+  }
+]);
