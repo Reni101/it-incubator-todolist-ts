60ea5c3f07e67fb86f9efe283ccb70d6e6e64dc9
Maxim Dmitriev <Renissance@tut.by>
2022-06-24 10:14:27 +0300

    TodoList - lesson 9 tasksReducer

--- src/Reducer/task-reducer.ts
+++ src/Reducer/task-reducer.ts
@@ -1,8 +1,10 @@
-import {TasksType} from "../App";
+import {TasksType, TaskType} from "../App";
+import {v1} from "uuid";
 
 type removeTaskACType = ReturnType<typeof removeTaskAC>
+type addTaskACType = ReturnType<typeof addTaskAC>
 
-type AllActions = removeTaskACType
+type AllActions = removeTaskACType | addTaskACType
 
 
 export const tasksReducer = (state: TasksType, action: AllActions): TasksType => {
@@ -10,6 +12,14 @@ export const tasksReducer = (state: TasksType, action: AllActions): TasksType =>
         case 'REMOVE-TODOLIST': {
             return {
                 ...state, [action.todolistId]: state[action.todolistId].filter(el => el.id !== action.taskId)
+            }
+        }
+        case 'ADD-TASK': {
+            const newTask: TaskType = {
+                id: v1(), title: action.title, isDone: false,
+            }
+            return {
+                ...state,[action.todolistId]: [newTask, ...state[action.todolistId]]
             }
         }
 
@@ -24,6 +34,14 @@ export const removeTaskAC = (taskId: string, todolistId: string) => {
     return {
         type: "REMOVE-TODOLIST",
         taskId,
+        todolistId,
+    } as const
+
+}
+export const addTaskAC = (title: string, todolistId: string) => {
+    return {
+        type: "ADD-TASK",
+        title,
         todolistId,
     } as const
 }
