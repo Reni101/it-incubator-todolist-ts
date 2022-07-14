acf11b944a26c5c3b7fe4dbfe1f9b3a1c1d25fdb
Maxim Dmitriev <Renissance@tut.by>
2022-07-14 10:25:06 +0300

    TodoList 12 storybook

--- src/stories/decorators/ReduxStoreProviderDecorator.tsx
+++ src/stories/decorators/ReduxStoreProviderDecorator.tsx
@@ -1,34 +1,9 @@
-import {Provider} from "react-redux";
-import React from 'react';
-import {combineReducers, legacy_createStore} from "redux";
-import {tasksReducer} from "../../Reducer/task-reducer";
-import {todoListsReducer} from "../../Reducer/todolists-reducer";
-import {v1} from "uuid";
+import React from 'react'
+import {Provider} from 'react-redux'
+import {store} from "../../Reducer/store";
 
-const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todoListsReducer
-})
 
-const initialGlobalState = {
-    todolists: [
-        {id: 'todolistId1', title: 'What to learn', filter: 'all'},
-        {id: 'todolistId2', title: 'What to buy', filter: 'all'}
-    ],
-    tasks: {
-        ['todolistId1']: [
-            {id: v1(), title: 'HTML&CSS', isDone: true},
-            {id: v1(), title: 'JS', isDone: true}
-        ],
-        ['todolistId2']: [
-            {id: v1(), title: 'Milk', isDone: true},
-            {id: v1(), title: 'React Book', isDone: true}
-        ]
-    }
-}
+export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactComponentElement<any>) => {
 
-export const storyBookStore = legacy_createStore(rootReducer)
-
-export const ReduxStoreProviderDecorator = (storyFn: any) => {
-    return <Provider store={storyBookStore}>{storyFn} </Provider>
+    return <Provider store={store}>{storyFn()}    </Provider>
 }
