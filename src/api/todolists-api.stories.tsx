0d55e7aa18c451d0f56d9edee6b7a00fa6138194
Maxim Dmitriev <Renissance@tut.by>
2022-08-19 21:38:28 +0300

    TodoList 13 Axios

--- src/api/todolists-api.stories.tsx
+++ src/api/todolists-api.stories.tsx
@@ -8,25 +8,32 @@ const instane = axios.create({
     }
 })
 
-export type TodoListType = {
-	id: string;
-	title: string;
-	addedDate: string;
-	order: number;
+type TodolistType = {
+    id: string
+    addedDate: string
+    order: number
+    title: string
+}
+
+export type ResponseType<D = {}> = {
+    resultCode: number
+    messages: Array<string>
+    fieldsErrors: Array<string>
+    data: D
 }
 
 
 export const todolistAPI = {
-    getTodolists(){
-       return instane.get('todo-lists')
+    getTodolists() {
+        return instane.get<TodolistType[]>('todo-lists')
     },
-    createTodolist(title:string){
-        return instane.post('todo-lists', {title})
+    createTodolist(title: string) {
+        return instane.post<ResponseType<{ item: TodolistType }>>('todo-lists', {title})
     },
-    deleteTodolist(todolistID:string){
-        return instane.delete(`todo-lists/${todolistID}`)
+    deleteTodolist(todolistID: string) {
+        return instane.delete<ResponseType>(`todo-lists/${todolistID}`)
     },
-    updateTodolistTitle(todolistID:string,title:string){
-        return instane.put(`todo-lists/${todolistID}`,{title})
+    updateTodolistTitle(todolistID: string, title: string) {
+        return instane.put<ResponseType>(`todo-lists/${todolistID}`, {title})
     }
 }
