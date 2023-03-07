ccedbd7009df6c8f60ecdc3f7754500dbb3d7947
Maxim Dmitriev <Renissance@tut.by>
2023-03-07 10:35:35 +0300

    Revert "error"
    This reverts commit f96c615e5023be428cf89b2804db3e15da990dfb.

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
+import {store} from "./Reducer/store";
+import {App} from "./app/App";
+import {HashRouter} from "react-router-dom";
 
-import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
-import './index.css'
-import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
-
-import { App } from './app/App'
-import { store } from './Reducer/store'
-import * as serviceWorker from './serviceWorker'
-
-const container = document.getElementById('root')
-const root = createRoot(container!)
 
 root.render(
-  <Provider store={store}>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </Provider>
-)
+    <Provider store={store}>
+        <HashRouter>
+            <App/>
+        </HashRouter>
+    </Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-serviceWorker.unregister()
+serviceWorker.unregister();
